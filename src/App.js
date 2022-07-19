aae253d52772182492e675fc2073299cf3c72e0d
Bagas Imam <bagasimam.mw@gmail.com>
2022-07-19 09:29:39 +0700

    push fitur slide

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import 'swiper/css';
-import Navbar from "./components/navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import { Outlet} from "react-router-dom";
 // import { BrowserRouter, Route, Outlet } from 'react-router-dom';
 import Footer from "./components/footer/Footer";
