d20736aee866ce21daf05fcf4c0541746b581b9f
Bagas Imam <bagasimam.mw@gmail.com>
2022-07-19 08:50:50 +0700

    add slider

--- src/containers/TrendingSeriesList.js
+++ src/containers/TrendingSeriesList.js
@@ -1,61 +1,55 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Box, Typography } from "@mui/material";
-import { motion, useMotionValue } from "framer-motion";
+import { motion } from "framer-motion";
 
 import tmdb from "../apis/tmdb";
 import MovieCard from "../components/movieCard/MovieCard";
-import './TrendingSeriesList.css';
-
+import "./TrendingSeriesList.css";
 
 const TrendingSeriesList = () => {
-    const [movies, setMovies] = useState([]);
-    const [width, setWidth] = useState(0);
-    const carousel = useRef();
+	const [movies, setMovies] = useState([]);
+	const [width, setWidth] = useState(0);
+	const carousel = useRef();
 
-    useEffect(() => {
-        const fetchMovies = async () => {
-          try {
-            const fatchedMovies = await tmdb.get("trending/tv/week");
-            setMovies(fatchedMovies.data.results);
-          }catch (error) {
-            console.log(error);
-          }
-        };
+	useEffect(() => {
+		const fetchMovies = async () => {
+			try {
+				const fatchedMovies = await tmdb.get("trending/tv/week");
+				setMovies(fatchedMovies.data.results);
+			} catch (error) {
+				console.log(error);
+			}
+		};
 
-        fetchMovies();
-      }, []);
+		fetchMovies();
+	}, []);
 
-    useEffect(() => {
-      console.log(carousel.current.scrollWidth, carousel.current.offsideWidh)
-      setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
-    },[])
+	useEffect(() => {
+		console.log(carousel.current.scrollWidth, carousel.current.offsideWidh);
+		setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+	}, []);
 
-  return (
-    <>
-      <Box className={"series-list"}>
-        <div className={"series-titleBox"}>
-          <Typography className={"series-title"} variant="h3" component="h3">
-            Trending Series
-          </Typography>
-        </div>
-        <motion.div ref={carousel}>
-          <motion.div
-            drag="x"
-            dragConstraints={{right: 0, left: -1597}}
-            
-            className="series-carousel"
-          >
-            {movies.map((movie)=>(
-              // <SeriesCard key={movie.title} movie={movie}></SeriesCard>
-              <motion.div className="series-item">
-                <MovieCard key={movie.title} movie={movie}></MovieCard>
-              </motion.div>
-            ))}
-            </motion.div>
-          </motion.div>
-      </Box>
-    </>
-  );
+	return (
+		<>
+			<Box className={"series-list"}>
+				<div className={"series-titleBox"}>
+					<Typography className={"series-title"} variant="h3" component="h3">
+						Trending Series
+					</Typography>
+				</div>
+				<motion.div ref={carousel}>
+					<motion.div drag="x" dragConstraints={{ right: 0, left: -1597 }} className="series-carousel">
+						{movies.map((movie) => (
+							// <SeriesCard key={movie.title} movie={movie}></SeriesCard>
+							<motion.div className="series-item">
+								<MovieCard key={movie.title} movie={movie}></MovieCard>
+							</motion.div>
+						))}
+					</motion.div>
+				</motion.div>
+			</Box>
+		</>
+	);
 };
 
 export default TrendingSeriesList;
